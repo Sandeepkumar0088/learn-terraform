c713ea7568727f59c85bfef80d878434866cdaae
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-14 15:20:53 +0530

    Terraform variables

--- variables-1/main.tf
+++ variables-1/main.tf
@@ -10,3 +10,20 @@ output "env" {
 output "env123" {
     value = "${var.env}123"
 }
+
+variable "instance_count" {
+    type = number
+    default = 11
+}
+
+output "instance_count" {
+    value = var.instance_count
+}
+
+output "instance_count_1" {
+    value = "${var.instance_count+1}"
+}
+
+output "instance_count_merge" {
+    value = "${var.instance_count}"."${var.instance_count_1}"
+}
