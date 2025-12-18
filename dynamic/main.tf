8fbb5cb4038433555aaa73f59fb6ee31064e5f69
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-18 19:02:11 +0530

    terraform dynamic block

--- dynamic/main.tf
+++ dynamic/main.tf
@@ -8,4 +8,10 @@ resource "aws_security_group" "ex" {
         protocol        ="-1"
         cidr_blocks     = ["0.0.0.0/0"]
     }
+    ingress {
+            from_port       = 0
+            to_port         = 0
+            protocol        ="-1"
+            cidr_blocks     = ["0.0.0.0/0"]
+    }
 }
