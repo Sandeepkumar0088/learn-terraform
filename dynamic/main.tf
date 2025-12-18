9b20f8b88794623844bbcc519dad9d3c10f09ec3
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-18 13:53:20 +0530

    terraform dynamic block

--- dynamic/main.tf
+++ dynamic/main.tf
@@ -3,9 +3,9 @@ resource "aws_security_group" "ex" {
     description     = "allow tls inbound traffic and all outbound traffic"
 
     egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    ="-1"
-        cidr        = ["0.0.0.0/0"]
+        from_port       = 0
+        to_port         = 0
+        protocol        ="-1"
+        cidr_blocks     = ["0.0.0.0/0"]
     }
 }
