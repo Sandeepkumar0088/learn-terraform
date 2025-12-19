e0589f54f3ae6783fe18155bedadefb8e3a26ac9
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-19 07:21:31 +0530

    terraform conditions

--- conditions/main.tf
+++ conditions/main.tf
@@ -1,7 +1,23 @@
-variable "speed" {
-    type=number
+# variable "speed" {
+#     type=number
+# }
+#
+# output "speed"{
+#     value = var.speed>60? "Too High" : "Too Low"
+# }
+
+resource "aws_instance" "main" {
+    count  = var.ec2? 1:0
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = var.type=="small"? "t3.small" : "t3.micro"
+    tags = {
+        Name = "test"
+    }
 }
 
-output "speed"{
-    value = var.speed>60? "Too High" : "Too Low"
+variable "ec2" {
+    default=false
+}
+variable "type" {
+    default = "micro"
 }
