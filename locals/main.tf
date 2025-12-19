ef3b69a9dd11ac57bc86a020ffc22dae231032fe
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-19 06:41:19 +0530

    terraform locals

---
+++ locals/main.tf
@@ -0,0 +1,21 @@
+variable "fruits" {
+    default = {
+        apple = {
+            price= 100
+        },
+        banana = {
+            price = 200
+        },
+        citrus = {
+            price = 300
+        }
+    }
+}
+
+locals {
+    fruit_prices = {for x,y in var.fruits:x=>y.price}
+}
+
+output "prices" {
+    values : locals.fruit_prices
+}
