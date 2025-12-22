efa0fd60a682d1be4f54c340e781ae8e4539b52c
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-22 08:00:22 +0530

    practicing terraform

--- servers-provisioners-v2/main.tf
+++ servers-provisioners-v2/main.tf
@@ -50,7 +50,7 @@ resource "null_resource" "terraform" {
     inline = [
       "sudo dnf install python3.13-pip -y",
       "sudo pip3.13 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/Sandeepkumar0088/roboshop-ansible-roles-v2.git main.yml -e component=${each.key}"
+      "ansible-pull -i localhost, -U https://github.com/Sandeepkumar0088/roboshop-ansible-roles-v2.git main.yml -e component=${each.key} -e env=dev"
     ]
   }
 }
