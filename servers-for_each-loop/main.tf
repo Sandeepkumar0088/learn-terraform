e79d60975c57775ebc06c38e841dafecd34dd7bc
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-22 06:50:13 +0530

    practicing terraform

---
+++ servers-for_each-loop/main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "main" {
+  for_each                = var.components
+
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = var.vpc_security_group_ids
+
+  tags = {
+    Name                  = "${each.key}"
+  }
+}
+
+resource "aws_route53_record" "record" {
+  for_each                = var.components
+
+  zone_id                 = var.zone_id
+  name                    = "${each.key}-dev"
+  type                    = "A"
+  ttl                     = 30
+  records                 = [ aws_instance.main[each.key].private_ip ]
+}
